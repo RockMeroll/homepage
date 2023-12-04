aa99996b8da6dd9e38b99801021fc429485dd101
RockMeroll <rockmeroll@hotmail.com>
2023-12-04 13:43:08 +0800

    Update list.jsx

--- src/components/services/list.jsx
+++ src/components/services/list.jsx
@@ -1,14 +1,11 @@
 import classNames from "classnames";
 
-import { columnMap } from "../../utils/layout/columns";
-
 import Item from "components/services/item";
 
 export default function List({ group, services, layout }) {
   return (
     <ul
       className={classNames(
-        // layout?.style === "row" ? `grid ${columnMap[layout?.columns]} gap-x-2` : "flex flex-col",
         "flex gap-4 justify-center flex-wrap",
         "mt-3 services-list",
       )}
